bec8e590c89a40b095026c7e95c99f526686c8b4
Dilip K <dilipkrishnappa@HweServer.mirafra.com>
2022-08-04 23:11:19 +0530

    single command 11:47am

---
+++ ls.sh
@@ -0,0 +1,4 @@
+# comment
+ls -lh
+#//using -h it will shows all the total size of files and directories and the individual size asize of each files and directories in human readable format
+echo "list all the file and directories size"
